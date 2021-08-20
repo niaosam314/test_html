d1d0cb3966152b884d2c083098335d4ee512eb07
niaosam314 <89257487+niaosam314@users.noreply.github.com>
2021-08-20 22:48:54 +0900

    Add files via upload

---
+++ javas.js
@@ -0,0 +1,10 @@
+const read = prompt("私が考えている数字は何でしょう?(1~10)")
+if (read==3.14 ){
+    alert("正解")
+ }else if (read >3.14){
+    alert("大きい!!")
+ }else if(read<3.14){
+    alert("少ない!!")
+ }else if(read!==3.14){
+     alert("数字を入れてください")
+ }
